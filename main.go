751bb3eb5f4fe9e941c81c32c21557cb7ef19bb3
Xiaohan Song <xiaohan.song@oracle.com>
2020-06-19 17:03:54 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/gofiber/fiber"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/ip", func(ctx *fiber.Ctx) {
+		if ctx.Get("X-Forwarded-For") != "" && ctx.Query("x-forwarded-for") != "false" {
+			ctx.SendString(ctx.Get("X-Forwarded-For"))
+		}
+
+		ctx.SendString(ctx.IP())
+	})
+
+	port := 8088
+	var err error
+	if os.Getenv("PORT") != "" {
+		port, err = strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			panic(err)
+		}
+	}
+	app.Listen(port)
+}
